df880589de560be6682e289164904d615c865d58
Leon Rische <leon.rische@me.com>
2017-09-02 12:44:11 +0200

    Add 'append' & 'reverse' list methods

--- src/builtin/list.rs
+++ src/builtin/list.rs
@@ -17,4 +17,45 @@ pub fn load(hm: &mut HashMap<String, Value>) {
             _ => Err(InvalidTypeOfArguments)
         }
     }));
+
+    register(hm, "append", Rc::new(|vs| {
+        check_arity!(vs, 2);
+        match vs[0] {
+            Value::Nil => {
+                Ok(vs[1].clone())
+            },
+            Value::List(ref elems) => {
+                match vs[1] {
+                    Value::List(ref elems2) => {
+                        let mut new_elems = elems.clone();
+                        new_elems.extend(elems2.iter().cloned());
+                        Ok(Value::List(new_elems))
+                    },
+                    Value::DottedList(ref elems2) => {
+                        let mut new_elems = elems.clone();
+                        new_elems.extend(elems2.iter().cloned());
+                        Ok(Value::DottedList(new_elems))
+                    },
+                    ref other => {
+                        let mut new_elems = elems.clone();
+                        new_elems.push(other.clone());
+                        Ok(Value::DottedList(new_elems))
+                    }
+                }
+            },
+            _ => Err(InvalidTypeOfArguments)
+        }
+    }));
+
+    register(hm, "reverse", Rc::new(|vs| {
+        check_arity!(vs, 1);
+
+        match vs[0] {
+            Value::List(ref elems) => {
+                let mut new_elems = elems.iter().rev().cloned().collect();
+                Ok(Value::List(new_elems))
+            },
+            _ => Err(InvalidTypeOfArguments),
+        }
+    }));
 }
