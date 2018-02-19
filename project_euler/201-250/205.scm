bed0260522195d09639bdf697ad465a6ac005354
Leon Rische <leon.rische@me.com>
2018-02-20 00:09:00 +0100

    Solve 205

--- project_euler/201-250/205.scm
+++ project_euler/201-250/205.scm
@@ -1,4 +1,4 @@
-; Solved 
+; Solved: 14.2.2018
 
 (defn total (n sides (acc 0))
       (if (zero? n)
@@ -15,15 +15,28 @@
       [(> a b) #t]
       [else (pete-wins?)])))
 
+(def colin-res (make-vector 37 0))
+
+(defn vector-inc! (id n)
+  (vector-set! id n (inc (vector-ref id n))))
+
+(~> (combinations~ 6 (list 1 2 3 4 5 6))
+    (map~ sum)
+    (each~ &(vector-inc! colin-res &1)))
+
+(defn count-wins (pete)
+  (~>
+    (range~ 0 (dec pete))
+    (map~ &(vector-ref colin-res &1))
+    sum~))
+
+(def total-wins
+  (~>
+    (combinations~ 9 (list 1 2 3 4))
+    (map~ sum)
+    (map~ &(count-wins &1))
+    sum~))
 
 (~>
-  (combinations~ 9 (list 1 2 3 4))
-  (map~ sum)
-  (map~
-    (fn (pete)
-        (~> (combinations~ 6 (list 1 2 3 4 5 6))
-            (map~ sum)
-            (count~ &(> pete &1)))))
-  sum~
-  println)
-
+  (/ (number->float total-wins) (pow 6 6) (pow 4 9))
+  solution)
