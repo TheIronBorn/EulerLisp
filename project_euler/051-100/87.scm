0cc2271de25f78d5279ad4af9cfb63361f7d80df
Leon Rische <leon.rische@me.com>
2018-02-19 22:31:30 +0100

    Move bitvector to stdlib

--- project_euler/051-100/87.scm
+++ project_euler/051-100/87.scm
@@ -23,30 +23,16 @@
     (map square)
     (select &(<= &1 max-n))))
 
-; Assuming the system supports 64-bit integers
-(def bitarray (make-vector (inc (div max-n 64)) 0))
-(defn insert! (n)
-  (let ([idx (div n 64)])
-  (vector-set!
-    bitarray
-    idx
-    (bitwise-or
-      (vector-ref bitarray idx)
-      (<< 1 (% n 64))))))
+(def bv (make-bitvector max-n))
 
 (each
   (fn (a)
-    (each (fn (b)
-        (~>
-          p2
-          (map &(+ a b &1))
-          (select &(< &1 max-n))
-          (each insert!)))
+    (each
+      (fn (b)
+        (~> (map &(+ a b &1) p2)
+            (select &(< &1 max-n))
+            (each &(bitvector-set! bv &1))))
      p3))
   p4)
 
-(~>
-  (range~ 0 (dec (vector-length bitarray)))
-  (map~ &(popcount (vector-ref bitarray &1)))
-  sum~
-  solution)
+(solution (bitvector-popcount bv))
