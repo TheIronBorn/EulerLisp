e01d1ba881da1a7825d469bd4611d76b0bbffc7d
Leon Rische <leon.rische@me.com>
2018-02-20 22:20:21 +0100

    m

--- project_euler/051-100/100_T.scm
+++ project_euler/051-100/100_T.scm
@@ -1,18 +1,31 @@
-; Solved: 
+; Solved: 20.2.2018
 
-(defn find-b (n)
-  (div (+ 1 (isqrt (+ (* 2 n n) (* -2 n) 1))) 2))
+; (defn find-b (r)
+;   (+ r 0.5 (sqrt (+ 0.25 (* 2 r r)))))
 
-(defn solves? (n b)
-  (= (* 2 b (- b 1))
-     (* n (- n 1))))
+; (defn solve (r)
+;   (let ([b (find-b r)])
+;     (if (integral? b)
+;       (println (cons r b)))
+;     (solve (inc r))))
+;
+;                     r    b
+;                   ==========
+;                     6   15
+;  6 + 2 * 15 - 1  = 35   85 = 15 + 70 = 15 + 2 * 35
+; 35 + 2 * 85 - 1 = 204  493 = 85 + 408 = 2 * 204
+;
+; => r_new = r + 2b - 1
+; => b_new = b + 2r_new
+;
+; TODO: This outputs the correct solution,
+; but I'd like to prove why that works
 
-(defn loop (from)
-  (let ([b (find-b (number->float from))])
-    (cond
-      [(solves? from b) (println "n: " from " b: " b)]
-      [(solves? from (inc b)) (println "n: " from " b: " (inc b))]
-      [else (loop (inc from))])))
+(defn step (r b)
+  (let* ([r-new (+ r (dec (* 2 b)))]
+         [b-new (+ b r-new r-new)])
+    (if {{r-new + b-new} > 1_000_000_000_000}
+      b-new
+      (step r-new b-new))))
 
-(def from (pow 10 12))
-(loop from)
+(solution (step 6 15))
