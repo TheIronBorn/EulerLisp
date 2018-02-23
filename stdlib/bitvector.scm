1ff0e3824957e7645c5c1578647315af56886b11
Leon Rische <leon.rische@me.com>
2018-02-23 22:15:00 +0100

    Add bitvector-get method

--- stdlib/bitvector.scm
+++ stdlib/bitvector.scm
@@ -1,5 +1,6 @@
 ; Assuming the system supports 64-bit integers
 (defn make-bitvector (len) (make-vector (inc (div len 64)) 0))
+
 (defn bitvector-set! (bv n)
   (let ([idx (div n 64)])
   (vector-set!
@@ -8,8 +9,14 @@
     (bitwise-or
       (vector-ref bv idx)
       (<< 1 (% n 64))))))
+
+(defn bitvector-get (bv n)
+  (let ([idx (div n 64)])
+    (!= 0 (bitwise-and
+            (vector-ref bv idx)
+            (<< 1 (% n 64))))))
+
 (defn bitvector-popcount (bv)
-  (~>
-    (range~ 0 (dec (vector-length bv)))
-    (map~ &(popcount (vector-ref bv &1)))
-    sum~))
+  (~> (range~ 0 (dec (vector-length bv)))
+      (map~ &(popcount (vector-ref bv &1)))
+      sum~))
